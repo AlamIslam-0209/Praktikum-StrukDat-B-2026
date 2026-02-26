029837bd1b7bdbcd0d51c6314d247cadc15a265d
Alam <alamganteng8250@gmail.com~>
2026-02-26 19:29:44 +0700

    menambah latihan praktikum 3

---
+++ Islam Rahmatan LilAlamin/Pertemuan-3/Latihan/Object.py
@@ -0,0 +1,11 @@
+#Class merupakan blueprint untuk membuat objek
+class MyClass: #membuat class dengan nama MyClass
+    x = 5 # yang memiliki atribut x dengan nilai 5
+
+p1 = MyClass() # membuat objek p1 dari class MyClass
+p2 = MyClass() # membuat objek p2 dari class MyClass
+p3 = MyClass() # membuat objek p3 dari class MyClass
+
+print(p1.x) # Output: 5
+print(p2.x) # Output: 5
+print(p3.x) # Output: 5
